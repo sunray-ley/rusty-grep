ee67d8e98bee37d4ed70a7eb7d92c2be025cb75c
sunray-ley <sunray.ley@gmail.com>
2023-06-28 15:32:14 +0800

    refactor: format code

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,6 @@ fn main() {
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    println!("With text:\n{contents}");
+    println!("With text:");
+    println!("{}", contents);
 }
